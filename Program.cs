1ef5dc6f5d35f7a5b40c813cd28d555e0d288ad3
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:28:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+namespace Uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // 
+            Console.WriteLine("Du har lärt dig skriva ut tecken");
+        }
+    }
+}
